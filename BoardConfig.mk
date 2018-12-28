24f3aa3aa2865c9abc5a021d8d5879294d5f5eb9
mauronofrio <poronofrio@gmail.com>
2018-12-28 17:29:18 +0100

    nx606j: twrp: Starting using TOOLBOX instead of BUSYBOX
    Change-Id: I92303f4d5e11a281adc918b50dd42dab5754d2a1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,7 @@ TW_DEVICE_VERSION=$(shell date '+%Y%m%d.%H')
 TW_EXCLUDE_TWRPAPP := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_NTFS_3G := true
+TW_USE_TOOLBOX := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_THEME := portrait_hdpi
 
